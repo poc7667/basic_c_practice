8f444e41508fd248ac735115a15bf31738585c38
poc.hsu <poc7667@gmail.com>
2016-10-22 09:42:56 -0700

    update template

--- template_c_project/main.c
+++ template_c_project/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <errno.h>
+#include <string.h>
 
 int main(){
-    printf("hi");
+    errno = 0;
+    printf("Thank you and good bye");
+
     return 0;
 }
