fd94d76daad709dc893e5e3ae30fbc0db8982c37
poc.hsu <poc7667@gmail.com>
2016-10-29 16:05:33 -0700

    completed hw6-2

---
+++ hw6-2/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
+#include <string.h>
+
+const int NUM_OF_VALUES = 2;
+
+void showWelcomeMsg() {
+    printf("Please enter float number separated by spaces and press enter:");
+}
+
+static double* getUserInputFloats() {
+    double values[NUM_OF_VALUES];
+    while (1) {
+        errno = 0;
+        char temp[257];
+        fgets(temp, 256, stdin);
+        char *str = strtok(temp, " ");
+        int cnt = 0;
+        while (str != NULL && cnt < NUM_OF_VALUES) {
+            char *endTemp;
+            double val = strtod(str, &endTemp);
+            if (*endTemp == 0 || *endTemp == '\n') {
+                values[cnt] = val;
+                cnt++;
+                str = strtok(NULL, " ");
+            } else {
+                errno = 1;
+                break;
+            }
+        }
+        if (errno == 1) {
+            printf("\nError.Please enter again!");
+        } else {
+            break;
+        }
+    }
+    return values;
+}
+
+int main() {
+
+    showWelcomeMsg();
+    double *values = getUserInputFloats();
+    printf("You entered %lf %lf\n", values[0], values[1]);
+    printf("Thank you for giving your choice");
+
+    return 0;
+}
